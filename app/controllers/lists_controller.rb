9ea6249f4155d3ae8619ea438835e5cbe5974611
tracy sikenjana <unboxedcanvas@gmail.com>
2022-10-27 17:55:45 +0200

    Added Lists Controller

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,44 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+    redirect_to list_path(@list)
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
+
+# #As a user, I can see all my movie lists
+# As a user, I can create a movie list
+# As a user, I can see the details of a movie list
+# As a user, I can bookmark a movie inside a movie list
+# # As a user, I can destroy a bookmark
+
+# def create
+#   @restaurant = Restaurant.new(restaurant_params)
+#   @restaurant.save
+#   # No need for app/views/restaurants/create.html.erb
+#   redirect_to restaurant_path(@restaurant)
+# end
+
+# private
+
+# def restaurant_params
+#   params.require(:restaurant).permit(:name, :address, :rating)
+# end
